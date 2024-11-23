5b38365b96b0d3b7a9564ac6ee4ab496768355ee
D. RikaZ <119458243+dzakkirsolihin@users.noreply.github.com>
2024-11-23 18:25:38 +0700

    modifikasi route
    ubah landing page jadi ke dashboard, dan memindahkan link login register dari page welcome ke dashboard serta menambahkan logic mengubah login user menjadi profile ketika user sudah login

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,10 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/dashboard', function () {
+Route::get('/', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
